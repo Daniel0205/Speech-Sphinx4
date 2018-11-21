b243721287e0792d097e7a777a5c2fbd5c297f06
stebanca1998 <steban.cadena@correounivalle.edu.co>
2018-11-21 15:19:31 -0500

    Se añade la clase RecognizerDemo que al ejecutarla trata de reconocer el habla en tiempo real.

--- src/RecognizerDemo.java
+++ src/RecognizerDemo.java
@@ -1,29 +1,30 @@
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-
 import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import edu.cmu.sphinx.api.SpeechResult;
-import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
-public class TranscriberDemo {
-
+public class RecognizerDemo {
     public static void main(String[] args) throws Exception {
+
         Configuration configuration = new Configuration();
 
         configuration.setAcousticModelPath("resources/edu/cmu/sphinx/models/voxforge-es-0.1.1/model_parameters/voxforge_es_sphinx.cd_cont_1500");
         configuration.setDictionaryPath("resources/edu/cmu/sphinx/models/voxforge-es-0.1.1/etc/voxforge_es_sphinx.dic");
         configuration.setLanguageModelPath("resources/edu/cmu/sphinx/models/voxforge-es-0.1.1/etc/voxforge_es_sphinx.transcription.test.lm");
 
-        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
-        InputStream stream = new FileInputStream(new File("test.wav"));
+        LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+        // InputStream stream = new FileInputStream(new File("test.wav"));
 
-        recognizer.startRecognition(stream);
+        recognizer.startRecognition(true);
         SpeechResult result;
-        while ((result = recognizer.getResult()) != null) {
-            System.out.print(result.getHypothesis() +"\n");
-            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+        while (true) {
+            result = recognizer.getResult();
+            System.out.println(result);
+            String res = result.getHypothesis();
+            System.out.format("Hypothesis: %s\n", res);
+
+            if(res == "ROJO"){
+                break;
+            }
         }
         recognizer.stopRecognition();
     }
